d2f675b634f3cf171ecc2f7051ef3a601a875387
alitcaglar <aliturabicaglar@gmail.com>
2024-06-25 21:49:59 +0300

    vercel-error1-tr2

--- src/lib/models.ts
+++ src/lib/models.ts
@@ -1,46 +1,73 @@
-import mongoose from "mongoose";
+import clientPromise from "./connectToDb";
 
-const photoAlbumSchema = new mongoose.Schema({
-  photoName: {
-    type: String,
-    required: true,
-    unique: true,
-    maxLength: 20,
-  },
-  takenBy: {
-    type: String,
-    required: true,
-    maxLength: 20,
-  },
-  takenYear: {
-    type: String,
-    required: true,
-  },
-  photoUrl: {
-    type: String,
-    required: true,
-  },
-});
+export interface PhotoAlbum {
+  photoName: string;
+  takenBy: string;
+  takenYear: string;
+  photoUrl: string;
+}
 
-export const PhotoAlbum =
-  mongoose.models.PhotoAlbum || mongoose.model("PhotoAlbum", photoAlbumSchema);
+export interface User {
+  email: string;
+  address?: string;
+  role: "user" | "admin" | "atc266";
+}
 
-const UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+export async function getPhotoAlbumCollection() {
+  const client = await clientPromise;
+  const db = client.db();
+  return db.collection<PhotoAlbum>("PhotoAlbum");
+}
 
-  address: {
-    type: String,
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin", "atc266"],
-    default: "user",
-  },
-});
+export async function getUserCollection() {
+  const client = await clientPromise;
+  const db = client.db();
+  return db.collection<User>("User");
+}
 
-const User = mongoose.models.User || mongoose.model("User", UserSchema);
-export default User;
+// import mongoose from "mongoose";
+
+// const photoAlbumSchema = new mongoose.Schema({
+//   photoName: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//     maxLength: 20,
+//   },
+//   takenBy: {
+//     type: String,
+//     required: true,
+//     maxLength: 20,
+//   },
+//   takenYear: {
+//     type: String,
+//     required: true,
+//   },
+//   photoUrl: {
+//     type: String,
+//     required: true,
+//   },
+// });
+
+// export const PhotoAlbum =
+//   mongoose.models.PhotoAlbum || mongoose.model("PhotoAlbum", photoAlbumSchema);
+
+// const UserSchema = new mongoose.Schema({
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+
+//   address: {
+//     type: String,
+//   },
+//   role: {
+//     type: String,
+//     enum: ["user", "admin", "atc266"],
+//     default: "user",
+//   },
+// });
+
+// const User = mongoose.models.User || mongoose.model("User", UserSchema);
+// export default User;
