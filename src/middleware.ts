0f1796e7fa98697eac4a4eaf2d47687dcb061733
alitcaglar <aliturabicaglar@gmail.com>
2024-06-27 16:41:07 +0300

    dotenvadded7

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,7 +20,7 @@ export const config = {
   matcher: "/profile/login",
   runtime: "experimental-edge", // for Edge API Routes only
   unstable_allowDynamic: [
-    "/src/**", // allows a single file
+    "/src/lib/models.ts", // allows a single file
     "/node_modules/function-bind/**", // use a glob to allow anything in the function-bind 3rd party module
   ],
 };
